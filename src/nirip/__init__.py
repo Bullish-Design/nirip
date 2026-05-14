f9ff24cecb00c8ee52de01b670f58f032462ba8a
Bullish Design <BullishDesignLLC@gmail.com>
2026-05-14 14:38:30 -0400

    refactor(api): type public load/apply helpers

--- src/nirip/__init__.py
+++ src/nirip/__init__.py
@@ -1,25 +1,29 @@
 """nirip public API."""
 
 from nirip.config import NiripConfig
+from nirip.execution.models import ApplyResult
 from nirip.facade.async_nirip import AsyncNirip
 from nirip.facade.sync_nirip import SyncNirip
 from nirip.spec.loader import load_spec_from_dict, load_spec_from_file, load_spec_from_string
+from nirip.spec.models import SessionSpec
 
 
-def load_session(path: str) -> object:
+def load_session(path: str) -> SessionSpec:
     """Convenience loader wrapper."""
     return load_spec_from_file(path)
 
 
-def apply_session(spec: object) -> object:
+def apply_session(spec: SessionSpec) -> ApplyResult:
     """Convenience sync apply wrapper."""
     with SyncNirip() as nirip:
         return nirip.apply(spec)
 
 
 __all__ = [
+    "ApplyResult",
     "AsyncNirip",
     "NiripConfig",
+    "SessionSpec",
     "SyncNirip",
     "apply_session",
     "load_session",
